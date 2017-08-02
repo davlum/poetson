af2cba16bd5b66582e3819a6aa380b3f71848e1c
davlum <david.lum91@gmail.com>
2017-08-01 20:25:51 -0400

    Fixed requirements and deleted libs.

--- bin/activate.fish
+++ bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/davidlum/poetson_master/test"
+set -gx VIRTUAL_ENV "/Users/davlum/flask-app"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
@@ -52,8 +52,8 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
         set -l old_status $status
 
         # Prompt override?
-        if test -n "(test) "            
-            printf "%s%s" "(test) " (set_color normal)
+        if test -n "(flask-app) "            
+            printf "%s%s" "(flask-app) " (set_color normal)
         else
             # ...Otherwise, prepend env
             set -l _checkbase (basename "$VIRTUAL_ENV")
