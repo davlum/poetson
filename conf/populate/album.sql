e1f9a382dac543316535d0afa37ec10c5d526bf9
davlum <david.lum91@gmail.com>
2017-08-11 19:02:40 -0400

    Added inserts.

---
+++ conf/populate/album.sql
@@ -0,0 +1,6 @@
+﻿INSERT into album (nom_album) VALUES
+('Frágil'),
+('eSLAMex: primera antología de espoken word mexicano'),
+('Celebración'),
+('Huellas de Luz'),
+('La tierra prometida');
---
+++ conf/populate/idioma.sql
@@ -0,0 +1,8 @@
+﻿INSERT INTO idioma (nom_idioma) VALUES
+('Español'),
+('Zapoteco'),
+('Inglés'),
+('Náhuatl'),
+('Tojolabal'),
+('Totonaca'),
+('Portugués');
